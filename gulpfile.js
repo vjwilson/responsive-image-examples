b78ea2dcb3ff985d8a13fd29521fe0e4c052857c
Van J. Wilson <van.wilson1992@alumni.unc.edu>
2016-07-24 08:16:13 -0400

    Add index page for all examples, with gulp watching all directories

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,20 @@ var browserSync = require('browser-sync').create();
 
 var argv = require('yargs').argv;
 
-var webroot = argv.f || 'final';
+var webroot = './';
+var folder = argv.f || '';
+
+if (folder) {
+  webroot = webroot + folder + '/';
+}
 
 // Static server
 gulp.task('serve', function() {
-    browserSync.init({
-        server: {
-            baseDir: './' + webroot + '/'
-        }
-    });
-});
+  browserSync.init({
+      server: {
+          baseDir: webroot
+      }
+  });
 
+  gulp.watch(['./**/*.html', './**/*.css']).on('change', browserSync.reload);
+});
